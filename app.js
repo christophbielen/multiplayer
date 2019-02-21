2184ee46c4de47ac6e096f4b9c5f01407f2fb9f9
christophbielen <47751623+christophbielen@users.noreply.github.com>
2019-02-21 10:04:03 +0100

    all

--- app.js
+++ app.js
@@ -7,12 +7,7 @@ app.get('/', function(req, res){
 });
 app.use('/client',express.static(__dirname + '/client') );
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
-	
-app.listen(port, ip);
+serv.listen(2000);
 
 var SOCKET_LIST = {};
 
