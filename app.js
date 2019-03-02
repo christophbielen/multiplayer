98b2a286b46af4bf4a2a2c2bb1912236863773af
christophbielen <47751623+christophbielen@users.noreply.github.com>
2019-03-02 14:58:56 +0100

    appchanges

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+var mongojs = require("mongojs");
+var db = mongojs('localhost:27017/multiplayer', ['accounts']);
+
+const bcrypt = require('bcrypt');
+
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
@@ -5,51 +10,126 @@ var serv = require('http').Server(app);
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/client/index.html');
 });
-app.use('/client',express.static(__dirname + '/client') );
+
+app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(2000);
+console.log('Server started');
 
 var SOCKET_LIST = {};
 
-var USERS = {
-	//"username":"password"
-	"Admin":"adminpas",
+// functions
 
-};
-
-var isValidPassword = function(data){
-	return USERS[data.username] === data.password;
+// login functions
+var isValidPassword = function(data, cb){
+	db.accounts.findOne({login:data.username}, function(err, res){
+		if(res!=null){
+			if (res){
+				bcrypt.compare(data.password, res.password, function(err, result) {
+					if(result){
+						cb({success:true, reason:"success"});
+					}else {
+						cb({success:false, reason:"Not a valid password."});
+					}
+				});
+			}else {
+				cb({success:false, reason:"User does not exist"});
+			}
+		}else {
+			cb({success:false, reason:"User does not exist"});
+		}
+	});
 }
 
+//signup functions
+var isUsernameTaken = function(data, cb){
+	db.accounts.find({login:data.username}, function(err, res){
+		if(res.length > 0){
+			cb(true);
+		}else {
+			cb(false);
+		}
+	});
+}
+
+var addUser = function(data, cb){
+	
+	bcrypt.hash(data.password, 10, function(err, hash) {
+		// Store hash in database
+		db.accounts.insert({login:data.username, password:hash}, function(err){
+			cb();
+		});
+	});
+}	
+		
+
+
+//IO
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
-	var self = socket;
-	socket.id = Math.random();
-	socket.name = "Guest";
+	socket.id = (new Date().valueOf()*10000) + Math.round(Math.random()*10000);
 	
 	SOCKET_LIST[socket.id] = socket;
+		
 	
-	socket.emit('changeName', socket.name);
-	
-	socket.on('chatMsg', function(data){
-		var playerName = socket.name;
-		data = data.replace(/<(?:.|\n)*?>/gm, ''); 
-		for(var i in SOCKET_LIST){
-			SOCKET_LIST[i].emit('addToChat', {name:playerName, msg:data,});
+	// Login attempt
+	socket.on('signIn', function(data){
+		var t = true;
+		for (var i in SOCKET_LIST){
+			var socket = SOCKET_LIST[i];
+			if (socket.name == data.username){
+				t=false;
+			}
+		}
+		if (t){
+			isValidPassword(data, function(res){
+				if(res.success){
+					socket.emit('signInResponse', {success:true, username:data.username});
+					socket.name = data.username;
+				}else{
+					socket.emit('signInResponse', {success:false, reason:res.reason});
+				}
+			});	
+		} else {
+			socket.emit('signInResponse', {success:false, reason:"User is already logged in"});
 		}
 	});
 	
-	socket.on('login', function(data){
-		data.username = data.username.replace(/<(?:.|\n)*?>/gm, '');
-		data.password = data.password.replace(/<(?:.|\n)*?>/gm, '');
-		if(isValidPassword(data)){
-			socket.name = data.username;
-			socket.emit('changeName', socket.name);
-			socket.emit('loginResponse', {success:true});
-		}else{
-			socket.emit('loginResponse', {success:false});
+	//Signup attempt
+	socket.on('signUp', function(data){
+		isUsernameTaken(data, function(res){	
+			if(res){
+				socket.emit('signUpResponse', {success:false});
+			}else{
+				addUser(data, function(){
+					socket.emit('signUpResponse', {success:true});
+				});
+			}
+		});
+		
+	});
+	
+	
+	//chat
+	socket.on('chatMsg',function(data){
+		var chattext = data.chattext;
+		var textToSend = "";
+		
+		if(chattext.indexOf(this.name + ": ")==0){
+			textToSend = chattext.slice(this.name.length+2);
+		} else {
+			textToSend = chattext;
+		}
+		
+		for (var i in SOCKET_LIST){
+			var socketsend = SOCKET_LIST[i];
+			socketsend.emit('chatMsg', {sendname:this.name, chattext:textToSend, socketname: socketsend.name});
 		}
 	});
 	
+	//disconnect
+	socket.on('disconnect', function () {
+		delete SOCKET_LIST[socket.id];
+    });
 	
 });
--- client/js/client.js
+++ client/js/client.js
@@ -1,51 +1,167 @@
 var socket = io();
+var myUsername = "";
 
-//login
+// LOGIN 
+//Login Div variables
+var signContainerDiv = $('#signContainerDiv');
+var signDiv = $('#signDiv');
+var signDivUsername = $('#signDivUsername');
+var signDivPassword = $('#signDivPassword');
+var signDivSignInButton = $('#signDivSignInButton');
+var signDivSignUpButton = $('#signDivSignUpButton');
 
-var loginButton = document.getElementById("loginButton");
-var signupButton = document.getElementById("signupButton");
-var usernameInput = document.getElementById("usernameInput");
-var passwordInput = document.getElementById("passwordInput");
-var loginDiv = document.getElementById("loginDiv");
+// Chat Divs
+var chatDiv = $('#chatDiv');
+var chatTextDiv = $('#chatTextDiv');
+var chatInputDiv = $('#chatInputDiv');
+var chatInput = $('#chatInput');
 
-loginButton.onclick = function(){
-	socket.emit('login', {username:usernameInput.value, password:passwordInput.value});
-}
+chatDiv.hide();
 
-socket.on('loginResponse', function(data){
+//Game div
+var gameContainer = $('#game-container');
+
+
+//Login attempt 
+signDivUsername.on('keypress', function (e) {
+         if(e.which === 13){
+
+            //Disable textbox to prevent multiple submit
+            $(this).attr("disabled", "disabled");
+
+            signDivPassword.focus();
+			
+            //Enable the textbox again if needed.
+            $(this).removeAttr("disabled");
+         }
+   });
+ 
+signDivPassword.on('keypress', function (e) {
+         if(e.which === 13){
+
+            //Disable textbox to prevent multiple submit
+            $(this).attr("disabled", "disabled");
+
+            signDivSignInButton.click();
+			
+            //Enable the textbox again if needed.
+            $(this).removeAttr("disabled");
+         }
+}); 
+   
+signDivSignInButton.click(function(){
+	socket.emit('signIn', {username:signDivUsername.val(), password:signDivPassword.val()});
+});
+
+//Login response
+socket.on('signInResponse', function(data){
 	if(data.success){
-		loginDiv.style.display = "none";
-	} else {
-		alert("Login failed");
+		signContainerDiv.hide();
+		signDiv.fadeOut();
+		chatDiv.css("visibility","visible");
+		chatDiv.fadeIn();
+		chatInput.val(data.username + ": ");
+		chatInput.focus();
+		chatInput.attr('maxlength',data.username.length + 82);
+		chatTextDiv.append("Welcome to CBQuest!</br>");
+		gameContainer.fadeIn();
+	}else{
+		alert("Sign In Failed: " + data.reason);
 	}
 });
 
-//game
-var welcomeDiv = document.getElementById("welcomeDiv");
-var loginForm = document.getElementById("loginForm");
-var usernameInput = document.getElementById("usernameInput");
-var passwordInput = document.getElementById("passwordInput");
-var chatText = document.getElementById("chatText");
-var chatForm = document.getElementById("chatForm");
-var chatInput = document.getElementById("chatInput");
-
-socket.on('addToChat', function(data) {
-	chatText.innerHTML += ("<div class='name'>"+data.name+"</div><div class='chatmsg'>"+data.msg+"</div>");
-	chatText.scrollTop = chatText.scrollHeight;
+//Signup attempt
+signDivSignUpButton.click(function(){
+	socket.emit('signUp', {username:signDivUsername.val(), password:signDivPassword.val()});
 });
 
-chatForm.onsubmit = function(e){
-	e.preventDefault();
-	socket.emit('chatMsg', chatInput.value);
-	chatInput.value ="";
+//Signup response
+socket.on('signUpResponse', function(data){
+	if(data.success){
+		alert("Sign Up successful, you can sign in now.");
+	}else{
+		alert("Sign Up Failed. Username exists already.");
+	}
+});
+
+
+//CHAT
+chatInput.on('keypress', function (e) {
+         if(e.which === 13){
+
+            //Disable textbox to prevent multiple submit
+            $(this).attr("disabled", "disabled");
+
+            socket.emit('chatMsg', {chattext:chatInput.val()});
+
+            //Enable the textbox again if needed.
+            $(this).removeAttr("disabled");
+         }
+   });
+
+
+socket.on('chatMsg', function(data){
+	chatInput.val(data.socketname+ ": ");
+	
+	chatTextDiv.append("<b>"+data.sendname+":</b> <font color='blue'>"+data.chattext +"</font></br>");
+	chatTextDiv.animate({
+        scrollTop: chatTextDiv.get(0).scrollHeight
+    }, 2000);
+	chatInput.focus();
+});
+
+
+
+
+
+
+// PHASER
+
+const config = {
+  type: Phaser.AUTO,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor: "#222222",
+  parent: "game-container",
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  // "this" === Phaser.Scene
+  this.load.image("repeating-background", "https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/images/escheresque_dark.png");
 }
 
-socket.on('changeName', function(data){
-	welcomeDiv.innerHTML = "<p class='title'>Welcome "+data+"</p>";
+function create() {
+  // You can access the game's config to read the width & height
+  const { width, height } = this.sys.game.config;
+
+  // Creating a repeating background sprite
+  const bg = this.add.tileSprite(0, 0, width, height, "repeating-background");
+  bg.setOrigin(0, 0);
+
+  // In v3, you can chain many methods, so you can create text and configure it in one "line"
+  this.add
+    .text(width / 2, height / 2, "hello\nphaser 3\ntemplate", {
+      font: "100px monospace",
+      color: "white"
+    })
+    .setOrigin(0.5, 0.5)
+    .setShadow(5, 5, "#5588EE", 0, true, true);
+}
+
+function update(time, delta) {
+  // We aren't using this in the current example, but here is where you can run logic that you need
+  // to check over time, e.g. updating a player sprite's position based on keyboard input
+}
+
+gameContainer.hide();
+
+window.addEventListener('resize', () => {
+    game.resize(window.innerWidth, window.innerHeight);
 });
-
-loginForm.onsubmit = function(e){
-	e.preventDefault();
-	
-
-};
